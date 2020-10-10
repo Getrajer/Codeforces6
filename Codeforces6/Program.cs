ec25e3c5d8d51ced8944938f2703ee4412202d0e
getrajer533 <getrajer533@gmail.com>
2020-10-10 07:55:17 +0100

    Add project files.

---
+++ Codeforces6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GameWithSticks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int a = Convert.ToInt32(input[0]);
+            int b = Convert.ToInt32(input[1]);
+
+            int w = 0;
+
+            while(a != 0 && b != 0)
+            {
+                a--;
+                b--;
+
+                if (w == 0) w = 1; else w = 0;
+            }
+
+            if(w == 0) Console.WriteLine("Malvika"); else Console.WriteLine("Akshat");
+
+        }
+    }
+}
