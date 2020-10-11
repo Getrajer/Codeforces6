a1cb6618ec95cd68009a199e85d5228ac841b158
getrajer533 <getrajer533@gmail.com>
2020-10-11 17:44:35 +0100

    more

---
+++ 1335B-ConstructTheString/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _1335B_ConstructTheString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l = Convert.ToInt32(Console.ReadLine());
+
+            string[] words = new string[l];
+
+            for(int i = 0; i < l; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                int n = Convert.ToInt32(input[0]);
+                int a = Convert.ToInt32(input[1]);
+                int b = Convert.ToInt32(input[2]);
+
+                int asci = 97;
+                int counter = 0;
+
+                string word = "";
+
+                for(int j = 0; j < n; j++)
+                {
+                    if(counter == b)
+                    {
+                        asci = 97;
+                        counter = 0;
+                    }
+                    word += Convert.ToString((char)asci);
+                    asci++;
+                    counter++;
+                }
+
+                words[i] = word;
+            }
+
+            for (int i = 0; i < l; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+        }
+    }
+}
---
+++ 1374B-MultiplyBy2DivideBy6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _1374B_MultiplyBy2DivideBy6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] results = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                int s1 = 0;
+                int s2 = 0;
+
+                while(number%2 == 0)
+                {
+                    s1++;
+                    number /= 2;
+                }
+
+                while(number%3 == 0)
+                {
+                    s2++;
+                    number /= 3;
+                }
+
+                if(number == 1)
+                {
+                    if(s1 <= s2)
+                    {
+                        int answer = Math.Min(s1, s2);
+                        s1 -= answer;
+                        s2 -= answer;
+                        answer += s2 * 2;
+                        results[i] = answer;
+                    }
+                    else
+                    {
+                        results[i] = -1;
+                    }
+                }
+                else
+                {
+                    results[i] = -1;
+                }
+
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+
+        }
+    }
+}
