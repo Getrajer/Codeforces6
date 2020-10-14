ea80ad4bbc48d6be8cd40f1a9bd147c005ab2ec6
getrajer533 <getrajer533@gmail.com>
2020-10-14 07:35:42 +0100

    more

---
+++ 556A-CaseOfTheZerosAndOnes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _556A_CaseOfTheZerosAndOnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string input = Console.ReadLine();
+
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '0')
+                {
+                    if(i != 0)
+                    {
+                        if(input[i - 1] == '1')
+                        {
+                            input = input.Remove(i - 1, i);
+                            i--;
+                        }
+                        else if(input.Length - 1 != i)
+                        {
+                            if(input[i + 1] == '1')
+                            {
+                                input = input.Remove(i, i + 1);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(input.Length);
+
+        }
+    }
+}
