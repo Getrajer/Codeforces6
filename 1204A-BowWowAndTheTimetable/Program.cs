379ba772048a053938ce268b86176c0f0ec30bb3
getrajer533 <getrajer533@gmail.com>
2020-10-19 23:15:21 +0100

    more

---
+++ 1204A-BowWowAndTheTimetable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1204A_BowWowAndTheTimetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double decimal_number = 0;
+            double c = 0;
+            double t = 2;
+
+            for(int i = input.Length - 1; i >= 0; i--)
+            {
+                if(input[i] == '1')
+                {
+                    double n = Math.Pow(t, c);
+                    decimal_number += n;
+                }
+
+                c++;
+            }
+
+            double checker = 0;
+            t = 4;
+
+            while(decimal_number >= Math.Pow(t, checker))
+            {
+                checker++;
+            }
+
+            Console.WriteLine(checker);
+
+        }
+    }
+}
